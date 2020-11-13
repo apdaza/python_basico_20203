7758076c23bc1a11cb20a95eb9ffe186c9572475
apdaza <apdaza@gmail.com>
2020-11-13 18:59:21 -0500

    Inicio ejemplo notas

---
+++ 08 flask/notas/principal.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+# creacion de aplicacion flask
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estudiantes.sqlite3'
+app.config['SECRET_KEY'] = "12345qwerty"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#creacion objeto de bd
+db = SQLAlchemy(app)
+
+class Estudiante(db.Model):
+    id = db.Column('estudiante_id', db.Integer, primary_key = True)
+    nombre = db.Column(db.String(50))
+    apellido = db.Column(db.String(50))
+    codigo = db.Column(db.String(15))
+    correo = db.Column(db.String(50))
+
+    def __init__(self, datos):
+        self.nombre = datos['nombre']
+        self.apellido = datos['apellido']
+        self.codigo = datos['codigo']
+        self.correo = datos['correo'] 
+
+class Nota(db.Model):
+    id = db.Column('nota_id', db.Integer, primary_key = True)
+    valor = db.Column(db.Integer)
+    estudiante = db.Column(db.Integer)
+
+    def __init__(self, datos):
+        self.valor = datos["valor"]
+        self.estudiante = datos["estudiante"]
+
+@app.route('/')
+def index():
+    return render_template("index.html", rows = Estudiante.query.all())
+
+@app.route('/nuevo')
+def nuevo():
+    return render_template("nuevo.html")
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug = True)
